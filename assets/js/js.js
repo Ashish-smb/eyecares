8755f342dbf8313ce6545f7e0f44531a9be0ba85
Ashish-smb <ashishkumar2004smb@gmail.com>
2024-07-17 02:54:18 +0530

    half

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,7 +1,9 @@
-$('.our-service-carousel').owlCarousel({
-    items: 4,
-    margin:10,
+$('.partners').owlCarousel({
+    items: 6,
+    margin:20,
     autoplay:true,
     loop:true,
     autoplayTimeout:2000,
+    dots:true,
+    nav:true,
 });
