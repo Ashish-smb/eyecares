56c24526ca0cb0c49e47797619c1ebe44ab64378
Ashish-smb <ashishkumar2004smb@gmail.com>
2024-07-17 21:11:24 +0530

    pending-responsive

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,10 @@
+$(".testimonial").owlCarousel({
+    items: 1,
+    autoplay:true,
+    loop : true,
+    autoplayHoverPause: true,
+    dots:true
+})
 $('.partners').owlCarousel({
     items: 6,
     margin:20,
@@ -40,37 +47,64 @@ $('.doctor-carousel').owlCarousel({
 
 // popup javascript 
 (function($) {
-            $.fn.showsidebar = function(sel) {
-                $(this).click(function() {
-                    $(sel).addClass('show');
-                });
-                return this;
-            };
-
-            $.fn.hidesidebar = function(sel) {
-                $(this).click(function() {
-                    $(sel).removeClass('show');
-                });
-                return this;
-            };
-
-            $.fn.showpopup = function(t) {
-                const sel = $(this);
-                setTimeout(function() {
-                    sel.addClass('show');
-                }, t);
-            };
-        })(jQuery);
-
-        $(document).ready(function() {
-            $('#showpopup').showsidebar('.popup');
-
-            $('.popup-close').click(function() {
-                $('.popup').removeClass('show');
-            });
-
-            $('.popup').showpopup(2000);
+    $.fn.showsidebar = function(sel) {
+        $(this).click(function() {
+            $(sel).addClass('show');
         });
+        return this;
+    };
 
+    $.fn.hidesidebar = function(sel) {
+        $(this).click(function() {
+            $(sel).removeClass('show');
+        });
+        return this;
+    };
+
+    $.fn.showpopup = function(t) {
+        const sel = $(this);
+        setTimeout(function() {
+            sel.addClass('show');
+        }, t);
+    };
+})(jQuery);
+
+$(document).ready(function() {
+    $('#appointment').showsidebar('.popup');
+
+    $('#showpopup').showsidebar('.popup');
+
+    $('.popup-close').click(function() {
+        $('.popup').removeClass('show');
+    });
+
+    $('.popup').showpopup(2000);
+});
+
+// navbar
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#navbar').outerHeight();
+
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   
+   // Make sure they scroll more than delta
+   if(Math.abs(lastScrollTop - st) <= delta)
+       return;
+   
+   // If scrolled down and past the navbar, hide navbar
+   if (st > lastScrollTop && st > navbarHeight){
+       // Scroll Down
+       $('#navbar').css('top', -navbarHeight);
+   } else {
+       // Scroll Up
+       if(st + $(window).height() < $(document).height()) {
+           $('#navbar').css('top', 0);
+       }
+   }
+   
+   lastScrollTop = st;
+});
 
 
