3b024b806a601af98cf1570fc87bca214da4f47d
Ashish-smb <ashishkumar2004smb@gmail.com>
2024-07-17 16:33:16 +0530

    no-responsive

--- assets/js/js.js
+++ assets/js/js.js
@@ -6,4 +6,32 @@ $('.partners').owlCarousel({
     autoplayTimeout:2000,
     dots:true,
     nav:true,
+    responsive : {
+        0 : {
+            items:2
+        },
+        600 : {
+            items:4
+        },
+        900: {
+            items:5
+        }
+    }
+});
+
+$('.doctor-carousel').owlCarousel({
+    items:3,
+    loop:true,
+    margin :20,
+    responsive:{
+        0: {
+            items:1
+        },
+        600: {
+            items:2
+        },
+        900: {
+            items:3
+        }
+    }
 });
